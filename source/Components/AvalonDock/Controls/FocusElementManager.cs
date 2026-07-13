02e00cc6b8716f2d70251241e1dd58bd98fba8ef
gerleim <gerleim@gmail.com>
2026-07-13 20:48:50 +0200

    Revert "Guard Keyboard.Focus against disposed HWND in SetFocusOnLastElement"
    This reverts commit 714d6b919410452d099a0dbfb1a97e28576c6761.

--- source/Components/AvalonDock/Controls/FocusElementManager.cs
+++ source/Components/AvalonDock/Controls/FocusElementManager.cs
@@ -132,11 +132,7 @@ namespace AvalonDock.Controls
 			IInputElement objectToFocus;
 			if (_modelFocusedElement.GetValue(model, out objectToFocus))
 			{
-				if (objectToFocus is Visual v && PresentationSource.FromVisual(v) == null)
-					objectToFocus = null;
-
-				if (objectToFocus != null)
-					focused = objectToFocus == Keyboard.Focus(objectToFocus);
+				focused = objectToFocus == Keyboard.Focus(objectToFocus);
 			}
 
 			IntPtr handleToFocus;
